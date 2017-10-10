f3c7f8231a457c12b1e8824ad8a4c95610344d86
faqihsalban <faqih.salban@gmail.com>
2017-10-10 19:33:57 +0700

    testing

---
+++ android.kalkulator/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace android.kalkulator
+{
+    [Activity(Label = "android.kalkulator", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} click", count++);
+            };
+
+        }
+    }
+}
+
